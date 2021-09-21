60eeb14fd0353a23bc0c3d0b172158f3bb2d8277
alfredoPark-48 <parkalfredojeonghyun@gmail.com>
2021-09-20 19:23:04 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Input from "./components/Input";
+import Result from "./components/Result";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [equation, setEquation] = useState("");
+
+	const getEquation = (equationInput) => {
+		console.log(equation);
+		setEquation(equationInput);
+	};
+
+	return (
+		<div className="ui aligned basic segment">
+			<Input getEquation={getEquation} />
+			<div className="ui horizontal divider">{}</div>
+			<div className="ui section divider"></div>
+			{/* <Result renderedResult={equation} /> */}
+		</div>
+	);
+};
 
 export default App;
